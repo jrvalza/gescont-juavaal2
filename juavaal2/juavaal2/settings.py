dc8a27d08fe53c636a1fe0f955ec4a54671e91ce
jrvalza <jvalencia658@gmail.com>
2024-05-18 19:53:58 +0200

    https

--- juavaal2/juavaal2/settings.py
+++ juavaal2/juavaal2/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGIN_URL = 'http://37.187.181.98/juavaal2-api/appjuavaal2/not_logged_in/'
+LOGIN_URL = 'https://37.187.181.98/juavaal2-api/appjuavaal2/not_logged_in/'
 #LOGIN_URL = '/appjuavaal2/not_logged_in/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
