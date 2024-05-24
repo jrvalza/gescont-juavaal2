a8ba6663ba9a1f582fe790aded87ac9704d637ec
jrvalza <jvalencia658@gmail.com>
2024-05-24 14:59:28 +0200

    modif2

--- juavaal2/settings.py
+++ juavaal2/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGIN_URL = 'https://37.187.181.98/juavaal2-api/appjuavaal2/not_logged_in/'
+LOGIN_URL = 'https://vps-3206e74d.vps.ovh.net/juavaal2-api/appjuavaal2/not_logged_in/'
 #LOGIN_URL = '/appjuavaal2/not_logged_in/'
 
 # Default primary key field type
